708380f490afaf5075795c881b561232f40a4510
Gastón Zanitti <gzanitti@gmail.com>
2025-03-31 12:43:12 -0300

    Update crates/stark-backend/tests/fib_selector_air/air.rs
    Co-authored-by: Georg Wiese <georgwiese@gmail.com>

--- crates/stark-backend/tests/fib_selector_air/air.rs
+++ crates/stark-backend/tests/fib_selector_air/air.rs
@@ -41,11 +41,7 @@ impl<F: Field> BaseAir<F> for FibonacciSelectorAir {
     }
 }
 
-impl<F: Field> ColumnsAir<F> for FibonacciSelectorAir {
-    fn columns(&self) -> Option<Vec<String>> {
-        None
-    }
-}
+impl<F> ColumnsAir<F> for FibonacciSelectorAir {}
 
 impl<F: Field> BaseAirWithPublicValues<F> for FibonacciSelectorAir {
     fn num_public_values(&self) -> usize {
