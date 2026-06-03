4f3755ce7ea5e2940c313b3053c0de44feaf7847
Georg Wiese <georgwiese@gmail.com>
2026-06-03 17:42:15 +0200

    Revert "Remove ColumnsAir"
    This reverts commit 1d719aa9d5f3d2b18a2692bfbb55d009db7a3e4e.

--- crates/stark-backend/src/test_utils/dummy_airs/interaction/dummy_interaction_air.rs
+++ crates/stark-backend/src/test_utils/dummy_airs/interaction/dummy_interaction_air.rs
@@ -17,7 +17,7 @@ use crate::{
         stacked_pcs::stacked_commit, AirProvingContext, ColMajorMatrix, CommittedTraceData,
         CpuColMajorBackend,
     },
-    AirRef, PartitionedBaseAir, StarkProtocolConfig,
+    AirRef, ColumnsAir, PartitionedBaseAir, StarkProtocolConfig,
 };
 
 pub struct DummyInteractionCols;
@@ -65,6 +65,7 @@ impl DummyInteractionAir {
 }
 
 impl<F> BaseAirWithPublicValues<F> for DummyInteractionAir {}
+impl<F> ColumnsAir<F> for DummyInteractionAir {}
 impl<F> PartitionedBaseAir<F> for DummyInteractionAir {
     fn cached_main_widths(&self) -> Vec<usize> {
         if self.partition {
